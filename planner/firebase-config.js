09f55a7d7682632a72486b279855fa3a10d28ded
traditz <joemsprague@gmail.com>
2025-12-28 20:14:06 -0600

    Refactor firebase config for export and cleanup
    Updated firebaseConfig to use export syntax and removed optional measurementId.

--- planner/firebase-config.js
+++ planner/firebase-config.js
@@ -1,10 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// PATH: planner/firebase-config.js
+export const firebaseConfig = {
   apiKey: "AIzaSyDJYFPuNFgrhGCQQR6_X1IE4QqYDwZ6Vfk",
   authDomain: "dfwgv-planner.firebaseapp.com",
   projectId: "dfwgv-planner",
-  storageBucket: "dfwgv-planner.firebasestorage.app",
-  messagingSenderId: "699390463926",
-  appId: "1:699390463926:web:b47c0402e1b170c2233b17",
-  measurementId: "G-WFN0QSRYDH"
+  appId: "1:699390463926:web:b47c0402e1b170c2233b17"
 };
